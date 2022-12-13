8e5c8709540bd6742685d6bfc8a2faffa269a150
Ricardo Soto <ricardosoto@MacBook-Air-de-Ricardo.local>
2022-12-13 02:08:17 -0600

    Test base

--- src/Home.js
+++ src/Home.js
@@ -12,9 +12,9 @@ function Home() {
         <div style = {{marginTop: "10%"}}>
         
       <h1 >¡Bienvenida a Kfashion!</h1>
-      <h3>Tienda en línea de ropa</h3>
+      <h3>Venta de ropa en línea</h3>
       </div>
-      <a href="/Catalogo" className="btn btn-default btn-lg bg-dark text-white">Catálogo</a>
+      <a href="/Catalogo" className="btn btn-default btn-lg bg-dark text-white">Visitar nuestro catálogo</a>
       
     </div>
   }>

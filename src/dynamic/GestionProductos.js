95e91bdea8319c0c6a9a5d7b5f481b786313974e
Ricardo Soto <ricardosoto@MacBook-Air-de-Ricardo.local>
2023-01-04 16:33:33 -0600

    Arreglado el input xdddd

--- src/dynamic/GestionProductos.js
+++ src/dynamic/GestionProductos.js
@@ -444,18 +444,20 @@ function Gestion() {
               onChange={(e) => updateForm({ nombre: e.target.value })}
             ></input>
             <br />
+            
             <label className="">Categoria del producto</label>
             <br />
             <select
               value={selects}
               onChange={(e) => setSelects((form.categoria = e.target.value))}
               className="form-control w-25"
+              style={{position:"relative",left:"37%"}}
               aria-label="Default select example"
             >
               <option></option>
               {categorias}
             </select>
-            <br />
+            
             <label className="">Descripcion del producto</label>
             <br />
             <input
